90e723ab189e6f052e50b00850a13f19b2ab69d8
Michela-DC <micheladelconte@outlook.com>
2022-05-02 19:57:14 +0200

    Added Admin folder in controllers, updated web.php

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->name('admin.')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->group(function() {
+// con middleware('auth') specifico che a tutte le rotte di questo gruppo verrà applicato il middleware auth, quindi posso andare nell'home controller ed eliminare il costruttore che applica il middleware auth perché già lo applico qui
+// uso il metodo name() a cui passo il prefisso del nome della rotta, quindi non sarà necessario specificare admin nel nome della rotta, ovvero non dovrò scrivere ->name('admin.home')
+// uso il metodo prefix() a cui passo il prefisso del percorso delle rotte, quindi non sarà necessario specificare admin nel percorso, per esempio non dovrò più scrivere: Route::get('/admin/home', 'HomeController@index')->name('home');
+// uso il metodo namespace() per specificare che tutti i controller che si collegano a queste rotte si trovano nel name space Admin, invece che specificare ogni volta 'Admin\HomeController@index'
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::resource('posts', 'PostController');
+});
