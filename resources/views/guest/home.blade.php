2c914482e9ebf7d1c7719ea17cffa69339e8f948
Michela-DC <micheladelconte@outlook.com>
2022-05-09 22:02:47 +0200

    Added front.js and App.vue

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -20,8 +20,8 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="root">
-        Qui va montato tutto
-    </div>
+    <div id="root"></div>
+
+    <script src="{{asset('js/front.js')}}"></script>
 </body>
 </html>
